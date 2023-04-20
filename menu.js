0b3f1f629f423f9d6c8891f672ac0cfea830b9f3
lorruan <lo_pedro@hotmail.com>
2023-04-20 15:20:42 -0300

    mudando o menu responsivo

--- menu.js
+++ menu.js
@@ -1,15 +1,47 @@
-const btnMobile = document.getElementById('btn-mobile');
+const nav = document.querySelector(".nav");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
 
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
+function handleButtonClick(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  event.stopPropagation();
+  nav.classList.toggle("active");
+  handleClickOutside(menu, () => {
+    nav.classList.remove("active");
+    setAria();
+  });
+  setAria();
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement;
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute("data-target");
+      html.removeEventListener("click", handleHTMLClick);
+      html.removeEventListener("touchstart", handleHTMLClick);
+      callback();
+    }
+  }
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("click", handleHTMLClick);
+    html.addEventListener("touchstart", handleHTMLClick);
+    targetElement.setAttribute("data-target", "");
+  }
+}
+
+function setAria() {
+  const isActive = nav.classList.contains("active");
+  btnMenu.setAttribute("aria-expanded", isActive);
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
 
 
 /* CONTATO */
